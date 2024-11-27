f412e5fb0f0cbd4558cf1f0ee810ff8571212b8f
zzx <53366177+Zhangzxin24@users.noreply.github.com>
2024-11-27 14:44:35 +0800

    Delete MFC-PPI-main directory

--- MFC-PPI-main/run.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for i in range(10):
-    os.system(
-        "python model_train.py --ppi_path ./protein_info/protein.actions.SHS27k.STRING.pro2.txt --pseq ./protein_info/protein.SHS27k.sequences.dictionary.pro3.tsv --split random --p_feat_matrix ./protein_info/x_list.pt --p_adj_matrix ./protein_info/edge_list_12.npy --save_path ./result_save --epoch_num 500")
